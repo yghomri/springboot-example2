f69c6250f76a91dc4da1371fb72035a5f42cb4f3
Yacine <yghomri@uottawa.ca>
2022-09-18 03:39:20 +0000

    more code

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import com.example.demo.entities.Patient;
 import com.example.demo.repositories.PatientRepository;
@@ -26,32 +28,39 @@ public class DemoApplication implements CommandLineRunner {
         patientRepository.save(new Patient(null, "Freeman", new Date(), 100, false));
         patientRepository.save(new Patient(null, "Deniro", new Date(), 200, false));
         patientRepository.save(new Patient(null, "Rocky", new Date(), 300, true));
+        patientRepository.save(new Patient(null, "Batman", new Date(), 400, true));
+        patientRepository.save(new Patient(null, "superman", new Date(), 500, true));
 
-        System.out.println("******************");
+        System.out.println("*********1*********");
         patientRepository.findAll().forEach(p -> {
             System.out.println(p.toString());
         });
-        System.out.println("******************");
+        System.out.println("*********2*********");
         Patient patient = patientRepository.findById(3L).get();
         System.out.println(patient.toString());
-        System.out.println("******************");
-        List<Patient> patients = patientRepository.findByNameContains("De");
+        System.out.println("*********3*********");
+        Page<Patient> patients = patientRepository.findByNameContains("a", PageRequest.of(0, 2));
         patients.forEach(p -> {
             System.out.println(p.toString());
         });
-        System.out.println("******************");
+        System.out.println("*********4*********");
         List<Patient> patients2 = patientRepository.findBySick(true);
         patients2.forEach(p -> {
             System.out.println(p.toString());
         });
-        System.out.println("******************");
+        System.out.println("*********5*********");
         List<Patient> patients3 = patientRepository.findByNameContainsAndSick("F", false);
         patients3.forEach(p -> {
             System.out.println(p.toString());
         });
-
+        System.out.println("********6**********");
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(1, 3));
+        List<Patient> listPatients = pagePatients.getContent();
+        listPatients.forEach(p -> {
+            System.out.println(p.toString());
+        });
+        System.out.println("*********7*********");
         patientRepository.deleteById(2L);
-        System.out.println("******************");
         patientRepository.findAll().forEach(p -> {
             System.out.println(p.toString());
         });
--- demo/src/main/java/com/example/demo/repositories/PatientRepository.java
+++ demo/src/main/java/com/example/demo/repositories/PatientRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entities.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    public List<Patient> findByNameContains(String name);
+    public Page<Patient> findByNameContains(String name, Pageable pageable);
     public List<Patient> findBySick(boolean sick);
     public List<Patient> findByNameContainsAndSick(String name, boolean sick);
 }
